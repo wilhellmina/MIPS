b66139bed20ca42ee63027acc302b56869be437d
wilhellmina <x@wakiga.nl>
2021-07-15 12:40:44 +0900

    make programcounter work

--- programCounter.vhd
+++ programCounter.vhd
@@ -4,12 +4,18 @@ USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 ENTITY programCounter IS
 	PORT (
+		clk :in std_logic;
 		programCounterIn   : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 		programCounterOut  : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
 	);
 END programCounter;
 ARCHITECTURE Behavioral OF programCounter IS
 BEGIN
+	process(CLK)
+	begin
+	if rising_edge(CLK) then
 	programCounterOut <= programCounterIn;
+	end if;
+	end process;
 	-- programCounterOut <= x"00400000" OR std_logic_vector(to_unsigned(progamCounterIn * 4, 32));
 END Behavioral;
